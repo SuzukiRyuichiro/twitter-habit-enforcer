d514e6b62d3638d43e2ece3fef38cd1efd44d16d
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-29 01:45:43 +0900

    made some update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom_bootstrap.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
