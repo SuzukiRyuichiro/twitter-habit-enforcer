b269e109ec99cb3759cfc7602d589f1f41c5d5f2
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-29 18:00:35 +0900

    conditional class assignment

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -4,6 +4,7 @@ import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 
 const Habit = (props) => {
   const [habitCompletion, setHabitCompletion] = useState(false);
+  const habitContent = habitCompletion ? 'done text-muted' : 'not-done';
 
   return (
     <div className="habit-card">
@@ -11,7 +12,7 @@ const Habit = (props) => {
           <input type="checkbox" id={props.habit.id} onClick={() => setHabitCompletion(!habitCompletion)}/>
           <label for={props.habit.id}></label>
         </div>
-        <h2 className="habit-content">{ props.habit.content }</h2>
+        <h2 className={`habit-content ${habitContent}`}>{ props.habit.content }</h2>
         <FontAwesomeIcon icon={faTrashAlt} onClick={() => props.deleteHabit(props.habit)}/>
     </div>
   )
