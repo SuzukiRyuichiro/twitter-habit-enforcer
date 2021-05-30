83b4a1a9c5699603ec14cc8fe7b06c63e7a59050
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-05-30 16:48:24 +0900

    made the side nav prettier

--- src/components/side_nav.jsx
+++ src/components/side_nav.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import firebase from "firebase/app";
+
+const auth = firebase.auth();
+
+const closeNav = () => {
+  document.querySelector("#mySidenav").classList.remove("nav-open")
+}
 
 const SideNav = () => {
 
   //  Set the width of the side navigation to 0
-  const closeNav = () => {
-    document.querySelector("#mySidenav").classList.remove("nav-open")
-  }
 
   return (
     <div id="mySidenav" className="sidenav">
-      <a href="javascript:void(0)" className="closebtn" onClick={() =>closeNav()}>&times;</a>
-      <a href="#">Contact</a>
+      <a href="javascript:void(0)" className="closebtn" onClick={() => closeNav()}>&times;</a>
+      <SingOut />
     </div>
   )
 };
 
+function SingOut() {
+  const signOutAndClose = () => {
+    auth.signOut();
+    closeNav();
+  }
+
+  return (
+    auth.currentUser && (
+      <span className="sidenav-item" onClick={() => signOutAndClose()}>
+        Sign Out
+      </span>
+    )
+  );
+}
 
 
 export default SideNav;
