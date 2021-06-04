9ce5d9bb1e08bf0aa0435a29eac0a48599c45822
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-06-04 17:03:08 +0900

    made some update

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+
+
+
+exports.tweetTest = functions.https.onRequest((request, response) => {
+  const Twitter = require('twitter-lite');
+
+  const client = new Twitter({
+    consumer_key: functions.config().twitter.consumer_key,
+    consumer_secret: functions.config().twitter.consumer_secret,
+    access_token_key: '',
+    access_token_secret: ''
+  });
+
+
+  async function tweetThread(thread) {
+    let lastTweetID = "";
+    for (const status of thread) {
+      const tweet = await client.post("statuses/update", {
+        status: status,
+        in_reply_to_status_id: lastTweetID,
+        auto_populate_reply_metadata: true
+      });
+      lastTweetID = tweet.id_str;
+    }
+  }
+
+  const thread = ["First tweet", "Second tweet", "Third tweet"];
+  tweetThread(thread).catch(console.error);
+})
